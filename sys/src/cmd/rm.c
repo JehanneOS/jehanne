71ea62eb5b1e49d7828efd54fa5514d8fbc927ac
Giacomo Tesio <giacomo@tesio.it>
2016-12-26 01:16:31 +0100

    rm -e prints the return value of remove syscalls
    Some devices return useful info on specific file remove (eg #0/pid, #0/ppid...)
    so we need a tool to get such info.
    
    	rm -e '#0/pid' '#0/ppid'
    	#0/pid 65
    	#0/ppid 59

--- sys/src/cmd/rm.c
+++ sys/src/cmd/rm.c
@@ -12,11 +12,14 @@
 
 char	errbuf[ERRMAX];
 int	ignerr = 0;
+int	printerr = 0;
 
 void
-err(char *f)
+err(long e, char *f)
 {
-	if(!ignerr){
+	if(printerr){
+		print("%s %ulld\n", f, e);
+	} else if(!ignerr){
 		errbuf[0] = '\0';
 		errstr(errbuf, sizeof errbuf);
 		fprint(2, "rm: %s: %s\n", f, errbuf);
@@ -30,38 +33,39 @@ void
 rmdir(char *f)
 {
 	char *name;
+	long e;
 	int fd, i, j, n, ndir, nname;
 	Dir *dirbuf;
 
 	fd = open(f, OREAD);
 	if(fd < 0){
-		err(f);
+		err(-1, f);
 		return;
 	}
 	n = dirreadall(fd, &dirbuf);
 	close(fd);
 	if(n < 0){
-		err("dirreadall");
+		err(-1, "dirreadall");
 		return;
 	}
 
 	nname = strlen(f)+1+STATMAX+1;	/* plenty! */
 	name = malloc(nname);
 	if(name == 0){
-		err("memory allocation");
+		err(-1, "memory allocation");
 		return;
 	}
 
 	ndir = 0;
 	for(i=0; i<n; i++){
 		snprint(name, nname, "%s/%s", f, dirbuf[i].name);
-		if(remove(name) != -1)
+		if((e = remove(name)) != -1)
 			dirbuf[i].qid.type = QTFILE;	/* so we won't recurse */
 		else{
 			if(dirbuf[i].qid.type & QTDIR)
 				ndir++;
 			else
-				err(name);
+				err(e, name);
 		}
 	}
 	if(ndir)
@@ -70,8 +74,8 @@ rmdir(char *f)
 				snprint(name, nname, "%s/%s", f, dirbuf[j].name);
 				rmdir(name);
 			}
-	if(remove(f) == -1)
-		err(f);
+	if((e = remove(f)) == -1)
+		err(e, f);
 	free(name);
 	free(dirbuf);
 }
@@ -80,6 +84,7 @@ main(int argc, char *argv[])
 {
 	int i;
 	int recurse;
+	long e;
 	char *f;
 	Dir *db;
 
@@ -92,19 +97,25 @@ main(int argc, char *argv[])
 	case 'f':
 		ignerr = 1;
 		break;
+	case 'e':
+		printerr = 1;
+		break;
 	default:
-		fprint(2, "usage: rm [-fr] file ...\n");
+		fprint(2, "usage: rm [-fr|-e] file ...\n");
 		exits("usage");
 	}ARGEND
+	if(printerr)
+		ignerr = 0;
 	for(i=0; i<argc; i++){
 		f = argv[i];
-		if(remove(f) != -1)
+		e = remove(f);
+		if(e != -1 && (!printerr || e == 0))
 			continue;
 		db = nil;
 		if(recurse && (db=dirstat(f))!=nil && (db->qid.type&QTDIR))
 			rmdir(f);
 		else
-			err(f);
+			err(e, f);
 		free(db);
 	}
 	exits(errbuf);

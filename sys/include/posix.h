61f47bce83d4e70199ee4bc536245ddc37ae0c96
Giacomo Tesio <giacomo@tesio.it>
2017-04-20 01:01:47 +0200

    Fix posix.h license header
    LibPOSIX is distributed as Affero GNU General Purpose License version 3, as you can see in the code. This was just a typo.

--- sys/include/posix.h
+++ sys/include/posix.h
@@ -3,8 +3,8 @@
  *
  * Copyright (C) 2017 Giacomo Tesio <giacomo@tesio.it>
  *
- * Jehanne is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, version 3 of the License.
  *
  * Jehanne is distributed in the hope that it will be useful,
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with Jehanne.  If not, see <http://www.gnu.org/licenses/>.
  */
 
